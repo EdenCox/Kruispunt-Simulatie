a1fd8cd9bac436b9fc2b0ff892f31b3484d7afcc
J Hazenberg <J.Hazenberg001@gmail.com>
2016-11-15 15:30:09 +0100

    Server

---
+++ CrossRoad/CrossRoad/Road.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossRoad
+{
+    class Road
+    {
+        public int trafficLight{ get; set; }
+        public Status status { get; set;
+        }
+        public int count { get; set; }
+        public bool changed { get; set; }
+        public int milliSec { get; set; }
+
+        public Road(int trafficLight, Status status, int count) {
+            this.trafficLight = trafficLight;
+            this.status = status;
+            this.count = count;
+            this.changed = false;
+        }
+
+        
+    }
+}
---
+++ CrossRoad/CrossRoad/jsonObject.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossRoad
+{
+    public class State
+    {
+        public int trafficLight { get; set; }
+        public int count { get; set; }
+    }
+
+    public class StateQueue
+    {
+        public List<State> state { get; set; }
+    }
+
+    public class Wrapper
+    {
+        [JsonProperty("state")]
+        public StateQueue StateQueue { get; set; }
+    }
+}
