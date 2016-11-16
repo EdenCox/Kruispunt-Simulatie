fb45f7841430e7bb9705ff97d4a134a0b5c24635
J Hazenberg <J.Hazenberg001@gmail.com>
2016-11-16 11:47:21 +0100

    json implementatie, socket sluit nog niet af wanneer client de socket sluit

--- CrossRoad/CrossRoad/Connection.cs
+++ CrossRoad/CrossRoad/Connection.cs
@@ -91,30 +91,37 @@ namespace CrossRoad
             //wait until usefull data has been recieved
             while (!client.GetStream().DataAvailable && running) ;
 
-            byte[] buffer = new byte[client.Available];
             while (running) {
                 if (handshakeDone) {
+
+                    byte[] buffer = new byte[client.Available];
+                    if (buffer.Length == 0)
+                        continue; //connection is closed notify mainclass
+                    
                     client.GetStream().Read(buffer, 0, buffer.Length);
-                    List<byte[]> decodedMsg = new List<byte[]>();
-                    decodedMsg = decodeWebsocketFrame(buffer);
+                    //List<byte[]> decodedMsg = new List<byte[]>();
+                    //decodedMsg = decodeWebsocketFrame(buffer);
                     //try {
                     //    decodedMsg = decodeWebsocketFrame(buffer);
                     //}
 
                     //catch(Exception e) { Debug.Write(e); }
+                    string msg = GetDecodedData(buffer, buffer.Length);
 
-                    string testByte = "";
+                    /*string testByte = "";
                     foreach (byte[] b in decodedMsg) {
                         testByte += System.Text.Encoding.UTF8.GetString(b);
                     }
-                    Debug.WriteLine("Debug: MSG: " +testByte);
-                    //todo parse json.
-                    if (testByte != "")
+                    */
+                    Debug.WriteLine(System.Environment.NewLine + "Debug: MSG: " + msg + System.Environment.NewLine);
+                    
+                    
+                    if (msg != "" && msg != "\u0003�no reason given")
                     {
                         lock (queue)
                         {
 
-                            queue = JsonConvert.DeserializeObject<StateQueue>(testByte).state;
+                            queue = JsonConvert.DeserializeObject<StateQueue>(msg).state;
                             //queue.Add((jsonObject)JsonConvert.DeserializeObject(testByte));
                             this.stateChange = true;
                         }
@@ -122,6 +129,50 @@ namespace CrossRoad
                     
                 }
             }
+        }
+
+        public static string GetDecodedData(byte[] buffer, int length)
+        {
+            byte b = buffer[1];
+            int dataLength = 0;
+            int totalLength = 0;
+            int keyIndex = 0;
+
+            if (b - 128 <= 125)
+            {
+                dataLength = b - 128;
+                keyIndex = 2;
+                totalLength = dataLength + 6;
+            }
+
+            if (b - 128 == 126)
+            {
+                dataLength = BitConverter.ToInt16(new byte[] { buffer[3], buffer[2] }, 0);
+                keyIndex = 4;
+                totalLength = dataLength + 8;
+            }
+
+            if (b - 128 == 127)
+            {
+                dataLength = (int)BitConverter.ToInt64(new byte[] { buffer[9], buffer[8], buffer[7], buffer[6], buffer[5], buffer[4], buffer[3], buffer[2] }, 0);
+                keyIndex = 10;
+                totalLength = dataLength + 14;
+            }
+
+            if (totalLength > length)
+                throw new Exception("The buffer length is small than the data length");
+
+            byte[] key = new byte[] { buffer[keyIndex], buffer[keyIndex + 1], buffer[keyIndex + 2], buffer[keyIndex + 3] };
+
+            int dataIndex = keyIndex + 4;
+            int count = 0;
+            for (int i = dataIndex; i < totalLength; i++)
+            {
+                buffer[i] = (byte)(buffer[i] ^ key[count % 4]);
+                count++;
+            }
+            return System.Text.Encoding.UTF8.GetString(buffer, dataIndex, dataLength);
+            //return Encoding.ASCII.GetString(buffer, dataIndex, dataLength);
         }
 
         private List<byte[]> decodeWebsocketFrame(Byte[] bytes)
